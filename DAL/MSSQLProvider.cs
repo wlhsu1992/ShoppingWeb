da2d627680e31d4b4ebdc45a5d3c5ac7e7ba4b1c
WeiLin Hsu <wlhsu1992@hotmail.com>
2021-10-11 12:37:56 +0800

    加入MSSQL Client資料存取方法

---
+++ DAL/MSSQLProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingWeb.DAL
+{
+    public class MSSQLProvider
+    {
+        public DataTable SQLSelect(string sqlCommand)
+        {
+            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["localDB"];
+            string connectionString = settings.ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlDataAdapter adp = new SqlDataAdapter(sqlCommand, conn);
+                DataSet ds = new DataSet();
+                adp.Fill(ds);
+                return ds.Tables[0];
+            }
+
+        }
+    }
+}
